70d880d18a9bb302e3a73042feadc31c713efaf4
Chester Arellano <chesterarellano1@gmail.com>
2021-06-27 09:40:15 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import Head from 'next/head';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+import { string } from 'prop-types';
+
+
+export default function Home() {
+  const { register, handleSubmit, errors, reset } = useForm();
+  
+  const TableRow = ({row}) => (
+    <tr>
+      {data.students.map(e=> <td>e</td>)}
+    </tr>
+  )
+
+
+  async function onSubmitForm(values) {
+    console.log(values);
+    let emails = values.emails.split(",");
+
+    let stringParams = "";
+    emails.forEach(e => {
+      if (e === "") return;
+      stringParams = `${stringParams}tutor=${e}&`
+    });
+    console.log(stringParams);
+    stringParams = stringParams.slice(0, -1); 
+    let config = {
+      method: 'get',
+      url: `https://student-mgmt-backend.herokuapp.com/api/getcommonstudents?${stringParams}`,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    try {
+      const response = await axios(config);
+      console.log(response);
+      if (response.status == 200) {
+
+      }
+    } catch (err) { }
+  }
+
+  return (
+    <div class="ml-5 mt-5">
+      <form class="w-full max-w-xl" onSubmit={handleSubmit(onSubmitForm)} method="GET">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+
+            <label for="name" >
+              Tutors' Email (separated by comma)
+            </label>
+            <textarea class="w-full"
+              rows="5"
+              type="text"
+              name="name"
+              {...register("emails")}
+
+              placeholder="Full name"
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 mt-6 px-4 border border-blue-700 rounded">
+              Submit
+            </button>
+
+          </div>
+        </div>
+
+      </form>
+      <hr/>
+      <table>
+        <tr>
+          <th>Email</th>
+        </tr>
+        <TableRow row={} />
+      </table>
+
+    </div>
+  )
+}
